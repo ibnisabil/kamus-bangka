9fa3b965f73116df805f32a2e33c152883e9aad7
ibnisabil <ibnisabil002@gmail.com>
2025-10-27 01:25:06 +0700

    add logo tab

--- resources/views/layouts/public_layout.blade.php
+++ resources/views/layouts/public_layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kamus Bangka</title>
 
+    <link rel="icon" href="{{ asset('images/LOGO-DISPAR.png') }}" type="image/png">
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
